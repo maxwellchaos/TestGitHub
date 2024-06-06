c923bb3d09d699719a8868f85554ce9497eb9295
maxwellchaos <maxwellchaos@gmail.com>
2024-06-06 14:40:22 +0300

    добавлена просто кнопка

--- testProject/testProject/Form1.Designer.cs
+++ testProject/testProject/Form1.Designer.cs
@@ -28,13 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button
+            // 
+            this.button.Location = new System.Drawing.Point(355, 71);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(177, 114);
+            this.button.TabIndex = 0;
+            this.button.Text = "button1";
+            this.button.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button;
     }
 }
 
